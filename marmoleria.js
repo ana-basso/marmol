0a4f49fc0cbe4e68df4d2ca6014f0d03da418882
Ana Basso <analucia_basso@hotmail.com>
2023-10-24 22:31:18 -0300

    cambio

--- marmoleria.js
+++ marmoleria.js
@@ -7,21 +7,21 @@ let consulta = confirm("¿Te puedo ayudar con tu compra?")
 
 if (consulta === true) {
 
-    let producto = prompt("¿Que producto estarias necesitando?")
-    let material = prompt("En que material necesitas tu " + producto + " , \n 1. Granitos  \n 2. Marmoles")
+    let mercaderia = prompt("¿Que producto estarias necesitando?")
+    let material = prompt("En que material necesitas tu " + mercaderia + " , \n 1. Granitos  \n 2. Marmoles")
 
     while (material != 1 & material != 2) {
         alert("Numero invalido");
         material = prompt("Ingresa 1 para granitos y 2 para marmoles")
     }
 
-    let color = prompt("Elegi el color de tu " + producto + " \n 1. BLANCO \n 2. NEGRO \n 3. GRIS")
+    let color = prompt("Elegi el color de tu " + mercaderia + " \n 1. BLANCO \n 2. NEGRO \n 3. GRIS")
     switch (color) {
-        case "1": alert("Tenemos " + producto + " de color " + color)
+        case "1": alert("Tenemos " + mercaderia + " de color " + color)
             break;
-        case "2": alert("Tenemos " + producto + " de color " + color)
+        case "2": alert("Tenemos " + mercaderia + " de color " + color)
             break;
-        case "3": alert("Tenemos " + producto + " de color " + color)
+        case "3": alert("Tenemos " + mercaderia + " de color " + color)
             break;
         default:
             while (color != 1 & color != 2 & color != 3) {
@@ -39,45 +39,135 @@ if (consulta === true) {
 
 
 }
-const Productos = function (nombre, precio, stock) {
+const Producto = function (nombre, precio, stock) {
     this.nombre = nombre;
     this.precio = precio;
     this.stock = stock;
 }
-let p1 = new Productos("pileta johnson", 50000, 10)
-let p2 = new Productos("pileta mi pileta", 60000, 10)
-let p3 = new Productos("canilla", 70000, 10)
+let p1 = new Producto("pileta johnson", 50000, 10)
+let p2 = new Producto("pileta mi pileta", 60000, 10)
+let p3 = new Producto("canilla", 70000, 10)
 
 let complementarios = [p1, p2, p3]
 
-function filtrar() {
-    let clave = prompt("Ingresa el producto que desea buscar").trim().toUpperCase()
-    let filtro = complementarios.filter((x) => x.nombre.toUpperCase()).includes(clave.toUpperCase())
+const filtrarp = document.getElementById("filtrar");
+filtrarp.addEventListener("click", () => { filtrarProducto(); });
+
+const agregarp = document.getElementById("agregar");
+agregarp.addEventListener("click", () => { agregarProducto(); });
+
+function filtrarProducto() {
+    const body = document.querySelector("body");
+    const input = document.getElementById("filter").value
+
+    let clave = input.trim().toUpperCase();
+    let filtro = complementarios.filter((producto) => producto.nombre.toUpperCase().includes(clave));
+
     if (filtro.length > 0) {
 
-        console.table(filtro)
+        const container = document.createElement("div");
+        container.classList.add("card-container");
+
+        filtro.forEach((producto) => {
+            const card = document.createElement("div");
+            card.classList.add("card");
+
+            const nombre = document.createElement("h2");
+            nombre.textContent = producto.nombre;
+            card.appendChild(nombre);
+
+            const precio = document.createElement("p");
+            precio.textContent = "Precio: ${producto.precio}";
+            card.appendChild(precio);
+
+            const stock = document.createElement("p");
+            stock.textContent = "stock: ${producto.stock}";
+            card.appendChild(stock);
+
+            container.appendChild(card)
+        });
+
+        body.appendChild(container);
     }
     else {
-        alert("No se encontro ninguna coincidencia con:" + clave)
+        alert("No se encontro ninguna coincidencia con" );
     }
 }
-filtrar()
-function agrego() {
-    let nombre = prompt("Ingresa el  nombre del producto: ").trim()
-    let precio = parseFloat(prompt("Ingresa el  precio del producto: "))
-    let stock = parseInt(prompt("Ingresa el stock del producto:  "))
 
-    if (isNaN(precio) || isNaN(stock) || nombre === "") {
-        alert("valor invalido")
-        return;
-    }
-    
-    if (complementarios.some((x) => x.nombre === Productos.nombre)) {
-        alert("El producto ya existe")
-        return;
-    }
+function agregarProducto() {
 
-    let Productos = new Productos(nombre, precio, stock);
-    complementarios.push(Productos)
-    console.table(complementarios)
+    const form = document.createElement("form");
+
+    form.innerHTML = "
+
+        <label for= "nombre-input"> Nombre: </label>
+        <input id="nombre-input" type="text" required>
+
+        <label for="precio-input">Precio:</label>
+        <input id="precio-input" type="number" step="0,01" required>
+
+        <label for="stock-input">Stock:</label>
+        <input id="stock-input" type="number" step="1" required>
+
+        <button type="submit">Agregar</button>";
+
+
+        form.addEventListener("submit", function (event) {
+
+        event.preventDefault();
+
+        const nombreInput=document.getElementById("nombre-input").value.trim();
+        const precioInput=parseFloat(document.getElementById("precio-input").value);
+        const stockInput=parseInt(document.getElementById("stock-input").value);
+
+        if (isNaN(precioInput) || isNaN(stockInput) || nombreInput === "") {
+         alert("valor invalido")
+        return;
+        }
+
+        const producto= new Producto(nombreInput,precioInput,stockInput);
+
+        if (complementarios.some((elemento) => elemento.nombre ===producto .nombre)) {
+             alert("El producto ya existe");
+             return;
+        }
+
+        complementarios.push(Producto);
+
+        localStorage.setItem("productos",JSON.stringify(complementarios));
+        alert("se ha agregado el producto"${producto.nombre}" a la lista.");
+
+        console.table(complementarios);
+
+        const container = document.createElement('div');
+        container.classList.add('card-container');
+
+        complementarios.forEach(producto)=>{
+
+        const card= document.createElement('div');
+        container.classList.add('card');
+
+        const nombre = document.createElement('h2')
+        nombre.textContent=producto.nombre;
+        card.appendChild(nombre);
+
+        const precio = document.createElement('p')
+        precio.textContent='Precio: ${producto.stock}';
+        card.appendChild(precio);
+
+        const stock = document.createElement('p')
+        stock.textContent='Stock:${producto.stock}';
+        card.appendChild(stock);
+
+        container.appendChild(card);
+        });
+
+        const body= document.querySelector('body')
+        body.appendChild(container);
+        form.reset();
+        })
+
+        const body = document.querySelector('body');
+        body.appendChild(form);
+                     
 }
